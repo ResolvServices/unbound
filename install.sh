53e2b65157b65661491a62d59f88c94ba028ac35
Resolv Services <noc@resolv.services>
2023-03-23 11:36:33 -0300

    Adicionando dominios para resolver localmente

--- install.sh
+++ install.sh
@@ -12,4 +12,8 @@ printf "\n\tBaixando arquivo de bloqueio de sites\n"
 
 wget https://raw.githubusercontent.com/ResolvServices/unbound/main/bloqueio.conf -O /etc/unbound/unbound.conf.d/bloqueio.conf
 
+printf "\n\tBaixando arquivo de domínios locais\n"
+
+wget https://raw.githubusercontent.com/ResolvServices/unbound/main/dominios.conf -O /etc/unbound/unbound.conf.d/bloqueio.conf
+
 printf "\n\tInstalação finalizada\n\tFavor realizar os testes e modificações\n\n"
