b84e80c19628f685b53e1ec298c650f725a8d6b8
Resolv Services <noc@resolv.services>
2023-03-23 11:08:47 -0300

    Script de instalação

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Script para instalação e pré-configuração do unbound
 ### Pré-requisitos
  - Debian 11
  - wget
+ 
+### Instalando
+Execute os seguintes comandos:
+
+wget https://raw.githubusercontent.com/ResolvServices/unbound/main/install.sh
+chmod +x install.sh
+./install.sh
---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+printf "\n\tInstalando servidor de DNS Unbound\n"
+
+apt install -y wget htop dnsutils unbound
+
+printf "\n\tBaixando arquivo base de configuração\n"
+
+wget https://raw.githubusercontent.com/ResolvServices/unbound/main/resolv.conf -O /etc/unbound/unbound.conf.d/resolv.conf
+
+printf "\n\tBaixando arquivo de bloqueio de sites\n"
+
+wget https://raw.githubusercontent.com/ResolvServices/unbound/main/bloqueio.conf -O /etc/unbound/unbound.conf.d/bloqueio.conf
+
+printf "\n\tInstalação finalizada\n\tFavor realizar os testes e modificações\n\n"
